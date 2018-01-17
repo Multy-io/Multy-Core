68b50367ec190bceb91af26cf40a2d546525d1fc
Pavel.K <pashaklybik@gmail.com>
2018-01-17 11:57:55 +0300

    MUL-523 add test ethereum transaction with data

--- multy_test/test_ethereum_transaction.cpp
+++ multy_test/test_ethereum_transaction.cpp
@@ -196,3 +196,63 @@ GTEST_TEST(EthereumTransactionTest, SmokeTest_testnet2)
             "f86c04850f0000000082520994d1b48a11e2251555c3c6d8b93e13f9aa2f51ea19882000000000000000802ba0098ee502619d5ba29d66b6c510265142f46ee0399456be7afb63ceefac0bd17ea07c19cc4145471b31f90af07f554611ac535cd006f64fb2141f1ed7bea7150386")),
             *serialied);
 }
+
+
+GTEST_TEST(EthereumTransactionTest, SmokeTest_testnet_withdata)
+{
+    AccountPtr account;
+    ErrorPtr error;
+    HANDLE_ERROR(make_account(
+                     CURRENCY_ETHEREUM,
+                     "5a37680b86fabdec299fa02bdfba8c9dfad08d796dc58c1d07527a751905bf71",
+                     reset_sp(account)));
+    ASSERT_NE(nullptr, account);
+
+    TransactionPtr transaction;
+    HANDLE_ERROR(make_transaction(account.get(), reset_sp(transaction)));
+    ASSERT_NE(nullptr, transaction);
+
+    const Amount balance("7500000000000000000");
+    const Amount value("1000");
+    const Amount gas_limit("121000");
+    const Amount gas_price("3000000000000");
+
+    {
+        const bytes payload = from_hex("ffff");
+        Properties& properties = transaction->get_transaction_properties();
+        properties.set_property("nonce", Amount("3"));
+        properties.set_property("chain_id", ETHEREUM_CHAIN_ID_RINKEBY);
+        properties.set_property("payload", to_binary_data(payload));
+    }
+
+    {
+        Properties& source = transaction->add_source();
+        source.set_property("amount", balance);
+    }
+
+    {
+        const bytes address = from_hex("d1b48a11e2251555c3c6d8b93e13f9aa2f51ea19");
+        Properties& destination = transaction->add_destination();
+        destination.set_property("address", to_binary_data(address));
+        destination.set_property("amount", value);
+    }
+
+    {
+        Properties& fee = transaction->get_fee();
+        fee.set_property("gas_price", gas_price);
+        fee.set_property("gas_limit", gas_limit);
+    }
+
+    transaction->update_state();
+    Amount estimated_fee = transaction->estimate_total_fee(1, 1);
+    std::cerr << "estimated_fee: " << estimated_fee.get_value() << "\n";
+
+    transaction->sign();
+    const BinaryDataPtr serialied = transaction->serialize();
+    std::cerr << "signed transaction:" << to_hex(*serialied) << "\n";
+
+    ASSERT_EQ(to_binary_data(from_hex(
+            "f86a038602ba7def30008301d8a894d1b48a11e2251555c3c6d8b93e13f9aa2f51ea198203e882ffff2ca0122bf1a37f949f0fc34354ca737eec7fd654e2172ecf893497d6e8356217512da05f01213f5d1c25d4b55e8c7219e572f92b00ec74a2662ae93c45928eb5133942")),
+            *serialied);
+}
+
