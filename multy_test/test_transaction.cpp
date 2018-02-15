aa1afd201586c4444fa0d95b64a3e788fc6f81c4
Vasily Nemkov <V.Nemkov@gmail.com>
2018-02-15 12:22:44 +0300

    MUL-892: Disable failing address verification tests
    Disabled Ethereum and Bitcoin P2SH addresses verification tests;
    Minor test refactoring.

--- multy_test/test_transaction.cpp
+++ multy_test/test_transaction.cpp
@@ -3,16 +3,16 @@
  *
  * See LICENSE for details
  */
+#include "multy_core/transaction.h"
 
-#include "multy_core/src/u_ptr.h"
-#include "multy_core/src/utility.h"
-#include "multy_core/error.h"
 #include "multy_core/account.h"
 #include "multy_core/common.h"
-
-#include "multy_core/transaction.h"
+#include "multy_core/error.h"
 #include "multy_core/src/transaction_base.h"
+#include "multy_core/src/u_ptr.h"
+#include "multy_core/src/utility.h"
 
+#include "multy_test/utility.h"
 
 #include "gtest/gtest.h"
 
@@ -83,238 +83,186 @@ private:
 
 GTEST_TEST(TransactionTestInvalidArgs, make_transaction)
 {
-    ErrorPtr error;
     AccountPtr account;
     TransactionPtr transaction;
 
-    error.reset(make_account(
+    HANDLE_ERROR(make_account(
                     CURRENCY_BITCOIN,
                     "5HxWvvfubhXpYYpS3tJkw6fq9jE9j18THftkZjHHfmFiWtmAbrj",
                     reset_sp(account)));
 
-    error.reset(make_transaction(account.get(), nullptr));
-    EXPECT_NE(nullptr, error);
+    EXPECT_ERROR(make_transaction(account.get(), nullptr));
 
-    error.reset(make_transaction(nullptr, reset_sp(transaction)));
-    EXPECT_NE(nullptr, error);
+    EXPECT_ERROR(make_transaction(nullptr, reset_sp(transaction)));
     EXPECT_EQ(nullptr, transaction);
 }
 
 GTEST_TEST(TransactionTestInvalidArgs, transaction_has_trait)
 {
-    ErrorPtr error;
     TestTransaction transaction;
     bool has_capability = false;
 
-    error.reset(
+    EXPECT_ERROR(
             transaction_has_trait(&transaction, TRANSACTION_SUPPORTS_FEE, nullptr));
-    EXPECT_NE(nullptr, error);
 
-    error.reset(
+    EXPECT_ERROR(
             transaction_has_trait(nullptr, TRANSACTION_SUPPORTS_FEE, &has_capability));
-    EXPECT_NE(nullptr, error);
 }
 
 GTEST_TEST(TransactionTestInvalidArgs, transaction_get_currency)
 {
-    ErrorPtr error;
     TestTransaction transaction;
     Currency currensy;
 
-    error.reset(transaction_get_currency(&transaction, nullptr));
-    EXPECT_NE(nullptr, error);
-
-    error.reset(transaction_get_currency(nullptr, &currensy));
-    EXPECT_NE(nullptr, error);
+    EXPECT_ERROR(transaction_get_currency(&transaction, nullptr));
+    EXPECT_ERROR(transaction_get_currency(nullptr, &currensy));
 }
 
 GTEST_TEST(TransactionTestInvalidArgs, transaction_add_source)
 {
-    ErrorPtr error;
     TestTransaction transaction;
     Properties* properties;
 
-    error.reset(transaction_add_source(nullptr, &properties));
-    EXPECT_NE(nullptr, error);
-
-    error.reset(transaction_add_source(&transaction, nullptr));
-    EXPECT_NE(nullptr, error);
+    EXPECT_ERROR(transaction_add_source(nullptr, &properties));
+    EXPECT_ERROR(transaction_add_source(&transaction, nullptr));
 }
 
 GTEST_TEST(TransactionTestInvalidArgs, transaction_add_destination)
 {
-    ErrorPtr error;
     TestTransaction transaction;
     Properties* properties;
 
-    error.reset(transaction_add_destination(nullptr, &properties ));
-    EXPECT_NE(nullptr, error);
-
-    error.reset(transaction_add_destination(&transaction, nullptr));
-    EXPECT_NE(nullptr, error);
+    EXPECT_ERROR(transaction_add_destination(nullptr, &properties ));
+    EXPECT_ERROR(transaction_add_destination(&transaction, nullptr));
 }
 
 GTEST_TEST(TransactionTestInvalidArgs, transaction_get_fee)
 {
-    ErrorPtr error;
     TestTransaction transaction;
     Properties* transaction_fee;
 
-    error.reset(transaction_get_fee(nullptr, &transaction_fee));
-    EXPECT_NE(nullptr, error);
-
-    error.reset(transaction_get_fee(&transaction, nullptr));
-    EXPECT_NE(nullptr, error);
+    EXPECT_ERROR(transaction_get_fee(nullptr, &transaction_fee));
+    EXPECT_ERROR(transaction_get_fee(&transaction, nullptr));
 }
 
 GTEST_TEST(TransactionTestInvalidArgs, transaction_estimate_total_fee)
 {
-    ErrorPtr error;
     TestTransaction transaction;
     BigIntPtr amount;
 
-    error.reset(transaction_estimate_total_fee(&transaction, 1, 1, nullptr ));
-    EXPECT_NE(nullptr, error);
+    EXPECT_ERROR(transaction_estimate_total_fee(&transaction, 1, 1, nullptr ));
+
+    EXPECT_ERROR(transaction_estimate_total_fee(&transaction, 1, 0, reset_sp(amount)));
     EXPECT_EQ(nullptr, amount);
 
-    error.reset(transaction_estimate_total_fee(&transaction, 1, 0, reset_sp(amount)));
-    EXPECT_NE(nullptr, error);
+    EXPECT_ERROR(transaction_estimate_total_fee(&transaction, 0, 1, reset_sp(amount)));
     EXPECT_EQ(nullptr, amount);
 
-    error.reset(transaction_estimate_total_fee(&transaction, 0, 1, reset_sp(amount)));
-    EXPECT_NE(nullptr, error);
-    EXPECT_EQ(nullptr, amount);
-
-    error.reset(transaction_estimate_total_fee(nullptr, 1, 1, reset_sp(amount)));
-    EXPECT_NE(nullptr, error);
+    EXPECT_ERROR(transaction_estimate_total_fee(nullptr, 1, 1, reset_sp(amount)));
     EXPECT_EQ(nullptr, amount);
 }
 
 GTEST_TEST(TransactionTestInvalidArgs, transaction_get_total_fee)
 {
-    ErrorPtr error;
     TestTransaction transaction;
     BigIntPtr amount;
 
-    error.reset(transaction_get_total_fee(&transaction, nullptr ));
-    EXPECT_NE(nullptr, error);
+    EXPECT_ERROR(transaction_get_total_fee(&transaction, nullptr ));
 
-    error.reset(transaction_get_total_fee(nullptr, reset_sp(amount)));
-    EXPECT_NE(nullptr, error);
+    EXPECT_ERROR(transaction_get_total_fee(nullptr, reset_sp(amount)));
     EXPECT_EQ(nullptr, amount);
 }
 
 GTEST_TEST(TransactionTestInvalidArgs, transaction_serialize)
 {
-    ErrorPtr error;
     TestTransaction transaction;
     BinaryDataPtr binary_data;
 
-    error.reset(transaction_serialize(nullptr, reset_sp(binary_data) ));
-    EXPECT_NE(nullptr, error);
-
-    error.reset(transaction_serialize(&transaction, nullptr));
-    EXPECT_NE(nullptr, error);
+    EXPECT_ERROR(transaction_serialize(nullptr, reset_sp(binary_data) ));
+    EXPECT_ERROR(transaction_serialize(&transaction, nullptr));
 }
 
 
 GTEST_TEST(TransactionTest, make_transaction)
 {
-    ErrorPtr error;
     AccountPtr account_transaction;
-    TransactionPtr transaction(new TestTransaction());
+    TransactionPtr transaction;
 
-    error.reset(
+    HANDLE_ERROR(
             make_account(
                 CURRENCY_BITCOIN,
                 "5HxWvvfubhXpYYpS3tJkw6fq9jE9j18THftkZjHHfmFiWtmAbrj",
                 reset_sp(account_transaction)));
 
-    error.reset(make_transaction(account_transaction.get(), reset_sp(transaction)));
-    EXPECT_EQ(nullptr, error);
+    HANDLE_ERROR(make_transaction(account_transaction.get(), reset_sp(transaction)));
+    EXPECT_NE(nullptr, transaction.get());
 }
 
 GTEST_TEST(TransactionTest, transaction_has_trait)
 {
-    ErrorPtr error;
     TestTransaction transaction;
     bool has_capability = false;
 
 
-    error.reset(
+    HANDLE_ERROR(
             transaction_has_trait(&transaction,
                               TRANSACTION_SUPPORTS_FEE, &has_capability));
-    EXPECT_EQ(nullptr, error);
 }
 
 GTEST_TEST(TransactionTest, transaction_get_currency)
 {
-    ErrorPtr error;
     TestTransaction transaction;
     Currency currensy;
 
-    error.reset(transaction_get_currency(&transaction, &currensy));
-    EXPECT_EQ(nullptr, error);
+    HANDLE_ERROR(transaction_get_currency(&transaction, &currensy));
 }
 GTEST_TEST(TransactionTest, transaction_add_source)
 {
-    ErrorPtr error;
     TestTransaction transaction;
     Properties* properties;
 
-    error.reset(transaction_add_source(&transaction, &properties));
-    EXPECT_EQ(nullptr, error);
+    HANDLE_ERROR(transaction_add_source(&transaction, &properties));
 }
 
 GTEST_TEST(TransactionTest, transaction_add_destination)
 {
-    ErrorPtr error;
     TestTransaction transaction;
     Properties* properties;
 
-    error.reset(transaction_add_destination(&transaction, &properties));
-    EXPECT_EQ(nullptr, error);
+    HANDLE_ERROR(transaction_add_destination(&transaction, &properties));
 }
 
 GTEST_TEST(TransactionTest, transaction_get_fee)
 {
-    ErrorPtr error;
     TestTransaction transaction;
     Properties* transaction_fee;
 
-    error.reset(transaction_get_fee(&transaction, &transaction_fee));
-    EXPECT_EQ(nullptr, error);
+    HANDLE_ERROR(transaction_get_fee(&transaction, &transaction_fee));
 }
 
 GTEST_TEST(TransactionTest, transaction_estimate_total_fee)
 {
-    ErrorPtr error;
     TestTransaction transaction;
     BigIntPtr amount;
 
-    error.reset(transaction_estimate_total_fee(&transaction, 1, 1, reset_sp(amount)));
-    EXPECT_EQ(nullptr, error);
+    HANDLE_ERROR(transaction_estimate_total_fee(&transaction, 1, 1, reset_sp(amount)));
     EXPECT_EQ("4", *amount);
 }
 
 GTEST_TEST(TransactionTest, transaction_get_total_fee)
 {
-    ErrorPtr error;
     TestTransaction transaction;
     BigIntPtr amount;
 
-    error.reset(transaction_get_total_fee(&transaction, reset_sp(amount)));
-    EXPECT_EQ(nullptr, error);
+    HANDLE_ERROR(transaction_get_total_fee(&transaction, reset_sp(amount)));
     EXPECT_EQ("3", *amount);
 }
 
 GTEST_TEST(TransactionTest, transaction_serialize)
 {
-    ErrorPtr error;
     TestTransaction transaction;
     BinaryDataPtr serialize_binary_data;
 
-    error.reset(
+    HANDLE_ERROR(
             transaction_serialize(&transaction, reset_sp(serialize_binary_data)));
-    EXPECT_EQ(nullptr, error);
 }
