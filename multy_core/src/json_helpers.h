4eabb6c9dc4c269c895b484b31041692e28688e0
Vasily Nemkov <V.Nemkov@gmail.com>
2018-07-20 14:26:20 +0300

    Linking multy_core against jsoncpp Added json_helpers.cpp\.h
    JSON CPP disabled tests

--- multy_core/src/json_helpers.h
+++ multy_core/src/json_helpers.h
@@ -1,11 +1,36 @@
-#ifndef JSON_HELPERS_H
-#define JSON_HELPERS_H
+/* Copyright 2018 by Multy.io
+ * Licensed under Multy.io license.
+ *
+ * See LICENSE for details
+ */
 
+#ifndef MULTY_CORE_SRC_JSON_HELPERS_H
+#define MULTY_CORE_SRC_JSON_HELPERS_H
 
-class json_helpers
+#include <string>
+#include <initializer_list>
+#include <utility>
+
+#include "json/json.h"
+
+namespace multy_core
 {
-public:
-    json_helpers();
-};
+namespace internal
+{
 
-#endif // JSON_HELPERS_H
+std::string to_string(const Json::Value& value);
+
+Json::Value make_json_object(std::initializer_list<std::pair<const char*, Json::Value>> object_values);
+Json::Value make_json_array(std::initializer_list<Json::Value> array_values);
+
+// Syntax sugar to reduce number of braces in make_json_array() :
+template <typename... Args>
+Json::Value make_json_array(Args... args)
+{
+    return make_json_array({args...});
+}
+
+} // namespace internal
+} // namespace multy_core
+
+#endif // MULTY_CORE_SRC_JSON_HELPERS_H
